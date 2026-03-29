40ed6bc6a78de44297ef25d014d9c80268b0bed4
JIMINLEEE <snsdvip0607@gmail.com>
2026-03-29 18:54:18 +0900

    Add files via upload
    5-2 44강

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Header from "./conponents/Header.jsx"; //확장자 안써고 괜찮
+import Main from "./conponents/Main.jsx";
+import Footer from "./conponents/Footer.jsx";
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Main />
+    <Footer />
+    </>
+  );
+};
+
+export default App;
